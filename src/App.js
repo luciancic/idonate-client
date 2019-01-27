d3a8a418b3a23a60eca1530758dbb3a96603158f
SireAlexander <chukwukaokafor13@gmail.com>
2019-01-26 22:29:32 -0500

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Header from './header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
