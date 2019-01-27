3dc8118891219f612bd28a923b688907fd0c336d
Lucian Condrea <luciancondreadev@gmail.com>
2019-01-26 23:49:58 -0500

    Move header to client pages, remove from home

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,18 +4,11 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class Navigation extends Component {
   render() {
     return (
-      
         <div className="App">
           <Link to="/donor">Donate</Link>
           <Link to="/receiver">Receive</Link>
           <Link to="/manager">Manage</Link>
-          
-          
-          
         </div>
-      
-
-
     );
   }
 }
