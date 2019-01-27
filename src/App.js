e17cd8279139aec9f2f04b5c0f4af1468dae7d6e
TheRealJT1408 <46851924+TheRealJT1408@users.noreply.github.com>
2019-01-26 23:26:38 -0500

    Create nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Donor from './Donor';
 import Receiver from './Receiver';
 import Manager from './Manager';
+import Navigation from './Navigation';
 import './App.css';
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
@@ -13,12 +14,10 @@ class App extends Component {
       <Router>
         <div className="App">
           <Header />
-          <Link to="/donor">Donate</Link>
-          <Link to="/receiver">Receive</Link>
-          <Link to="/manager">Manage</Link>
           <Route path="/donor" component={Donor} />
           <Route path="/receiver" component={Receiver} />
           <Route path="/manager" component={Manager} />
+          <Route path="/" exact component={Navigation} />
         </div>
       </Router>
     );
