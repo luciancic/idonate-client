b96f7781aa0bb3417318c550adf086c127019862
Lucian Condrea <luciancondreadev@gmail.com>
2019-01-26 22:02:56 -0500

    Add Router
    * Add basic router
    
    * Add base routes
    
    * Add 3 routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Donor from './Donor';
+import Receiver from './Receiver';
+import Manager from './Manager';
 import './App.css';
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Link to="/donor">Donate</Link>
+          <Link to="/receiver">Receive</Link>
+          <Link to="/manager">Manage</Link>
+          <Route path="/donor" component={Donor} />
+          <Route path="/receiver" component={Receiver} />
+          <Route path="/manager" component={Manager} />
+        </div>
+      </Router>
     );
   }
 }
